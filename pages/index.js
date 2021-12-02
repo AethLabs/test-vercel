3e91b7751b4704e61cbd60db57dc38cf16198344
Erik Hopp <me@erikhopp.com>
2021-12-02 19:23:51 +0000

    Remove unused variable

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,8 @@ import Link from 'next/link';
 // Vercel variable test
 console.log('process', process);
 console.log('process.env', process.env);
-console.log('Private process.env.ENV:', process.env.ENV);
-console.log('Private process.env.TEST_VALUE: ', process.env.TEST_VALUE);
 console.log('Public process.env.VERCEL_URL: ', process.env.VERCEL_URL);
+console.log('Private process.env.TEST_VALUE: ', process.env.TEST_VALUE);
 console.log(
   'Public process.env.NEXT_PUBLIC_TEST_VALUE: ',
   process.env.NEXT_PUBLIC_TEST_VALUE
@@ -16,9 +15,8 @@ const Test = props => {
     <>
       <h2>Variable test</h2>
       <ul>
-        <li>Private process.env.ENV: {process.env.ENV}</li>
-        <li>Private process.env.TEST_VALUE: {process.env.TEST_VALUE}</li>
         <li>Public process.env.VERCEL_URL: {process.env.VERCEL_URL}</li>
+        <li>Private process.env.TEST_VALUE: {process.env.TEST_VALUE}</li>
         <li>
           Public process.env.NEXT_PUBLIC_TEST_VALUE:
           {process.env.NEXT_PUBLIC_TEST_VALUE}
